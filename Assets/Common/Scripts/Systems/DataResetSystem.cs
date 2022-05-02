78c5f2825719565e59ac0f86fe9f673d10e507f8
FrAzenV2 <frazenv2@ya.ru>
2022-05-02 18:13:53 +0300

    Add prefabs

---
+++ Assets/Common/Scripts/Systems/DataResetSystem.cs
@@ -0,0 +1,13 @@
+﻿using Kuhpik;
+
+namespace Common.Scripts.Systems
+{
+    public class DataResetSystem : GameSystem
+    {
+        public override void OnUpdate()
+        {
+            game.InteractNextFrame = false;
+            game.RunNextFrame = false;
+        }
+    }
+}
