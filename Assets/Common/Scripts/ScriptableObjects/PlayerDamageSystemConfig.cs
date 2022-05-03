700e17a8fc2e962ae3402ed8bf42f117848168a1
FrAzenV2 <frazenv2@ya.ru>
2022-05-03 17:45:06 +0300

    Add damage on impact

--- Assets/Common/Scripts/ScriptableObjects/PlayerDamageSystemConfig.cs
+++ Assets/Common/Scripts/ScriptableObjects/PlayerDamageSystemConfig.cs
@@ -5,6 +5,8 @@ namespace Common.Scripts.ScriptableObjects
     [CreateAssetMenu(fileName = "New Player Damage System Config", menuName = "Configs/Systems/Player Damage System", order = 0)]
     public class PlayerDamageSystemConfig : ScriptableObject
     {
-        [field: SerializeField] public int LoseItemsAmount { get; private set; }= 1;
+        [field: SerializeField] public int LoseItemsAmountOnCatch { get; private set; } = 1;
+        [field: SerializeField] public int LoseItemsOnBump { get; private set; } = 1;
+        [field: SerializeField] public float SpeedToTakeBumpDamage { get; private set; } = 7;
     }
 }
