596e30683d7e4e7f87884a33da96838dc6fb316d
FrAzenV2 <frazenv2@ya.ru>
2022-05-03 19:52:59 +0300

    WIP

---
+++ Assets/Common/Scripts/UI/CurrencyScreen.cs
@@ -0,0 +1,27 @@
+﻿using DG.Tweening;
+using Kuhpik;
+using TMPro;
+using UnityEngine;
+
+namespace Common.Scripts.UI
+{
+    public class CurrencyScreen : UIScreen
+    {
+        [SerializeField] private TMP_Text moneyText;
+        private float currentMoney;
+        public override void Open()
+        {
+            base.Open();
+            Bootstrap.PlayerData.MoneyChanged += UpdateMoney;
+            UpdateMoney();
+        }
+        private void UpdateMoney()
+        {
+            DOTween.To(() => currentMoney, x => currentMoney = x,endValue: Bootstrap.PlayerData.Money, 0.3f)
+                .OnUpdate(() =>
+                {
+                    moneyText.text = ((int)currentMoney).ToString();
+                });
+        }
+    }
+}
